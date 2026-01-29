e8b4085ac9ffe547dd7c86f11dc7c9d21b498ddd
Cheesbert <jakob.silva@gmx.at>
2026-01-29 18:01:28 +0100

    Capture frames, change write ead

--- vp/src/platform/camera.cpp
+++ vp/src/platform/camera.cpp
@@ -5,24 +5,20 @@
 
 
 uint32_t* Camera::addr_to_reg(uint64_t addr) {
-    uint32_t* reg = nullptr;
-
     switch (addr) {
-        case CAPTURE_REG_CONTROL: reg = &control; break;
-        case CAPTURE_REG_WIDTH:   reg = &width;   break;
-        case CAPTURE_REG_HEIGHT:  reg = &height;  break;
-        case CAPTURE_REG_STATUS:  reg = &status;  break;
-        case CAPTURE_REG_INTERVAL: reg = &interval; break;
-        case CAPTURE_REG_JPEG_SIZE: reg = &jpeg_size; break; 
-        default: SC_REPORT_ERROR("Camera", "Invalid register address");
+        case CAPTURE_REG_CONTROL: return &control; 
+        case CAPTURE_REG_WIDTH:   return &width;  
+        case CAPTURE_REG_HEIGHT:  return &height;  
+        case CAPTURE_REG_STATUS:  return &status;  
+        case CAPTURE_REG_INTERVAL: return &interval; 
+        case CAPTURE_REG_JPEG_SIZE: return &jpeg_size; 
+        default: return nullptr; 
     }
-    return reg; 
+ 
 }
-
 void Camera::capture_thread() {
     while(true) {
-        // If control bit 0 is NOT set, wait for the event (triggered by write to control)
-        if (!(control & 0x1)) {
+        if (!(control & 0x1)) { // wait for event
             wait(capture_event);
         }
 
@@ -46,65 +42,46 @@ void Camera::capture_thread() {
 }
 
 void Camera::transport(tlm::tlm_generic_payload &trans, sc_core::sc_time &delay) {
-    auto addr = trans.get_address();
+    auto addr  = trans.get_address();
     auto cmd = trans.get_command();
     auto len = trans.get_data_length();
-    auto ptr = trans.get_data_ptr();
+    auto* ptr = trans.get_data_ptr();
 
-    if (addr < CAM_FRAME_BUFFER_SIZE) {
-        assert(cmd == tlm::TLM_READ_COMMAND);
-        assert((addr + len) <= CAM_FRAME_BUFFER_SIZE);
-            
+    std::cout << "TLM access addr=0x" << std::hex << addr << std::dec << std::endl;
+
+    if (addr < CAM_FRAME_BUFFER_SIZE) { // Framebuffer access
+        if (cmd != tlm::TLM_READ_COMMAND || addr + len > CAM_FRAME_BUFFER_SIZE) {
+            trans.set_response_status(tlm::TLM_ADDRESS_ERROR_RESPONSE);
+            return;
+        }
+
         memcpy(ptr, &framebuffer[addr], len);
         trans.set_response_status(tlm::TLM_OK_RESPONSE);
         return;
-    } 
-    if (len != 4) {
-        SC_REPORT_ERROR("Camera", "Register access must be 32-bit");
+    }
+
+    if (len != 4) { // Register
+        trans.set_response_status(tlm::TLM_BURST_ERROR_RESPONSE);
         return;
     }
-    auto it = addr_to_reg(addr);
 
-    if ((cmd == tlm::TLM_WRITE_COMMAND)) {
-        uint32_t value = *((uint32_t * ) ptr);
-        
-        if (addr == CAPTURE_REG_WIDTH) {
-            if (value > CAM_MAX_WIDTH) {
-                SC_REPORT_WARNING("Camera", "Invalid width ignored");
-                return; 
-            }
-        }else if (addr == CAPTURE_REG_HEIGHT) {
-            if (value > CAM_MAX_HEIGHT) {
-                SC_REPORT_WARNING("Camera", "Invalid height ignored");
-                return; 
-            }
-        }else if (addr == CAPTURE_REG_CONTROL) {
-            if (value & ~0x1) { // only bit 0 allowed
-                SC_REPORT_WARNING("Camera", "Invalid CONTROL bits ignored");
-                return;
-            }
-        }  
+    uint32_t* reg = addr_to_reg(addr);
+    if (!reg) {
+        trans.set_response_status(tlm::TLM_ADDRESS_ERROR_RESPONSE);
+        return;
     }
 
-    if (cmd == tlm::TLM_READ_COMMAND) {
-        *((uint32_t *) ptr) = *it;
-    } else if (cmd == tlm::TLM_WRITE_COMMAND) {
-        *it = *((uint32_t *)ptr);
-    } else {
-
+    if (cmd == tlm::TLM_READ_COMMAND) { // register to buffer
+        std::memcpy(ptr, reg, 4);
     }
+    else if (cmd == tlm::TLM_WRITE_COMMAND) { // buffer to register
+        std::memcpy(reg, ptr, 4);
 
-    if (cmd == tlm::TLM_WRITE_COMMAND) {
-        *it = *((uint32_t *)ptr); // Update the register value first
-
-        if (addr == CAPTURE_REG_CONTROL) {
-            if (*it & 0x1) { // If bit 0 (Start) was just written
-                capture_event.notify(sc_core::SC_ZERO_TIME);
+        if (addr == CAPTURE_REG_CONTROL && (*reg & 0x1)) {
+            capture_event.notify(sc_core::SC_ZERO_TIME);
         }
     }
-    
-    // Set response status (Crucial: fixes the assert error from earlier!)
+
     trans.set_response_status(tlm::TLM_OK_RESPONSE);
 
-    }
 }
--- vp/src/platform/tb_camera.cpp
+++ vp/src/platform/tb_camera.cpp
@@ -1,5 +1,8 @@
 #include <systemc>
 #include <fstream>
+#include <sys/stat.h>
+#include <cerrno>
+
 #include <tlm_utils/simple_target_socket.h>
 #include <tlm_utils/simple_initiator_socket.h>
 #include "camera.h"
@@ -9,20 +12,42 @@ SC_MODULE(CameraTB) {
     sc_core::sc_in<bool> irq;
 
     SC_CTOR(CameraTB) : isock("isock") {
-        SC_THREAD(run); // Registration
+        SC_THREAD(run);
         sensitive << irq;
     }
 
+    template <typename T>
+    void write(uint64_t addr, const T& value) {
+        static_assert(std::is_trivially_copyable_v<T>, "Data type must be trivial copyable");
+        
+        tlm::tlm_generic_payload trans; 
+        sc_core::sc_time delay = sc_core::SC_ZERO_TIME; 
+        size_t size = sizeof(T)
+
+        std::array<unsigned char, size> buffer{};
+        memcpy(buffer.data(), &value, size); // write source value to buffer
+
+        trans.set_command(tlm::TLM_WRITE_COMMAND); // cmd what to do
+        trans.set_address(addr);
+        trans.set_data_length(size); // number bytes transaction 4bytes 32bits 
+        trans.set_streaming_width(size);
+        trans.set_data_ptr(buffer.data());
+        trans.set_response_status(tlm::TLM_INCOMPLETE_RESPONSE); // transaction prepared but not completed 
+        
+        isock->b_transport(trans, delay);
+
+    } 
 
     void write32(uint64_t addr, uint32_t value) {
-        tlm::tlm_generic_payload trans; // Container Data
-        sc_core::sc_time delay = sc_core::SC_ZERO_TIME; // Keep track of simulation
+        tlm::tlm_generic_payload trans; 
+        sc_core::sc_time delay = sc_core::SC_ZERO_TIME; 
 
         trans.set_command(tlm::TLM_WRITE_COMMAND); // Writr Command
-        trans.set_address(addr); // Write to addr
-        trans.set_data_length(4); // 
+        trans.set_address(addr); // write to addr
+        trans.set_data_length(4); 
         trans.set_streaming_width(4);
         trans.set_data_ptr(reinterpret_cast<unsigned char*>(&value)); // Ptr to write value
+
         trans.set_response_status(tlm::TLM_INCOMPLETE_RESPONSE); // Incomcomplete atm 
 
         isock->b_transport(trans, delay); // Block until write has been done 
@@ -48,7 +73,7 @@ SC_MODULE(CameraTB) {
     }
 
     void saveFrame(uint32_t jpeg_size, std::string frameName) { 
-        std::ofstream ofs("test_frames/" + frameName + ".jpg", std::ios::binary);
+        std::ofstream ofs(frameName + ".jpg", std::ios::binary);
 
         if (!ofs) {
             std::cerr << "Error opening file\n";
@@ -82,19 +107,26 @@ SC_MODULE(CameraTB) {
         write32(Camera::CAPTURE_REG_CONTROL, start);
 
         uint32_t frames = 10; 
-        std::string frameName = "test_frame";
-        std::string saveName; 
+        std::string test_frames_dir = "test_frames"; 
+
+        // Create dir for test frames
+        if (mkdir(test_frames_dir.c_str(), 0755) && errno != EEXIST) {
+            perror("mkdir");
+        }
+
+        std::string frame_name = test_frames_dir + "/test_frame";
+        std::string save_name; 
 
         for (int i = 0; i < frames; i++) {
             wait(irq.posedge_event()); 
             
-            std::cout << "Receive frame" << i << "at: " << sc_core::sc_time_stamp() << std::endl; 
-            saveName = frameName = std::to_string(i); 
+            std::cout << "Received frame" << i << "at: " << sc_core::sc_time_stamp() << std::endl; 
+            save_name = frame_name + std::to_string(i); 
             jpeg_size = read32(Camera::CAPTURE_REG_JPEG_SIZE);
-            saveFrame(jpeg_size, saveName); 
-            std::cout << "Saved frame: " << frameName << " at " << sc_core::sc_time_stamp() << std::endl;
+            saveFrame(jpeg_size, save_name); 
+            std::cout << "Saved frame: " << frame_name << " at " << sc_core::sc_time_stamp() << std::endl;
         }
-        write32(0xFF000C, stop); 
+        write32(Camera::CAPTURE_REG_CONTROL, stop); 
         std::cout << "Simulation finished." << std::endl;
         sc_core::sc_stop();
     }
